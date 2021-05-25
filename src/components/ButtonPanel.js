c55b4ff6ff95f20f351bf569d33c116448931aa1
ZawadiSumba66 <rosesumba98@gmail.com>
2021-05-25 16:08:04 +0300

    Place the basic layout of the buttons

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from './Button';
+
+export default class ButtonPanel extends React.Component {
+  renderButton(i) {
+    return <Button name={i} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="calculator-row">
+          {this.renderButton('AC')}
+          {this.renderButton('+/-')}
+          {this.renderButton('%')}
+          {this.renderButton('/')}
+        </div>
+        <div className="calculator-row">
+          {this.renderButton(7)}
+          {this.renderButton(8)}
+          {this.renderButton(9)}
+          {this.renderButton('X')}
+        </div>
+        <div className="calculator-row">
+          {this.renderButton(4)}
+          {this.renderButton(5)}
+          {this.renderButton(6)}
+          {this.renderButton('-')}
+        </div>
+        <div className="calculator-row">
+          {this.renderButton(1)}
+          {this.renderButton(2)}
+          {this.renderButton(3)}
+          {this.renderButton('+')}
+        </div>
+        <div className="calculator-row">
+          {this.renderButton(0)}
+          {this.renderButton('.')}
+          {this.renderButton('=')}
+
+        </div>
+      </div>
+    );
+  }
+}
