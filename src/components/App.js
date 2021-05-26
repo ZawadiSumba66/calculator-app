a7ee48f671f64b03cb954531f988f02fcf2ec101
ZawadiSumba66 <rosesumba98@gmail.com>
2021-05-26 15:33:09 +0300

    Implement event handler in App componenet

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,31 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  calculate();
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      total : null,
+      next :null,
+      operation : null
+    }
+  }
+
+  handleClick(buttonName){
+    const data = {total,next,operation}
+    calculate(data,buttonName)
+    this.setState=({
+      total, next, operation
+    })
+  }
+  render(){
   return (
     <>
-      <Display />
-      <ButtonPanel />
+      <Display next={next} total={total} />
+      <ButtonPanel clickHandler={this.handleClick}/>
     </>
   );
+  }
 }
 
 export default App;
