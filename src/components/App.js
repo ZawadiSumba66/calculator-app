0c10f37c38f38f6df162d9b20a6f188967053cc5
ZawadiSumba66 <rosesumba98@gmail.com>
2021-05-26 16:26:42 +0300

    Establish a correct state logic

--- src/components/App.js
+++ src/components/App.js
@@ -3,30 +3,34 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component{
-  constructor(props){
-    super(props)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      total : null,
-      next :null,
-      operation : null
-    }
+      total: '',
+      next: '',
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(buttonName){
-    const data = {total,next,operation}
-    calculate(data,buttonName)
-    this.setState=({
-      total, next, operation
-    })
+  handleClick(buttonName) {
+    const calc = calculate(buttonName);
+    this.setState = ({
+      total: calc.total,
+      next: calc.next,
+      operation: calc.operation,
+    });
   }
-  render(){
-  return (
-    <>
-      <Display next={next} total={total} />
-      <ButtonPanel clickHandler={this.handleClick}/>
-    </>
-  );
+
+  render() {
+    const { next, total, operation } = this.state;
+    return (
+      <>
+        <Display next={next} total={total} operation={operation} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
   }
 }
 
