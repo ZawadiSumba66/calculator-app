363003bf15dd36fe18efbbb1c83b052cf3760d77
ZawadiSumba66 <rosesumba98@gmail.com>
2021-05-30 19:41:29 +0300

    Refactor code to use hooks

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,16 +1,14 @@
 import PropTypes from 'prop-types';
 
-function Display({ total, operation, next }) {
-  return (
-    <div>
-      <h2>
-        {total || '0'}
-        {operation || ''}
-        {next}
-      </h2>
-    </div>
-  );
-}
+const Display = ({ total, operation, next }) => (
+  <div>
+    <h2>
+      {total || '0'}
+      {operation || ''}
+      {next}
+    </h2>
+  </div>
+);
 
 Display.propTypes = {
   total: PropTypes.string.isRequired,
