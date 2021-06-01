5fbd80d02b96dc9416a800da107cf453ef46ff0c
ZawadiSumba66 <rosesumba98@gmail.com>
2021-06-01 21:23:27 +0300

    Fix linters errors

--- src/__tests__/components/Navbar.test.js
+++ src/__tests__/components/Navbar.test.js
@@ -1,19 +1,16 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom';
-
 
 it('should render the correct buttons for the calculator', () => {
-    
-    const { getByTestId } = render(
-        <Router>
-          <Navbar />
-        </Router>
-      );
-    expect(getByTestId('navbar')).toHaveTextContent('Math Magicians');
-    expect(getByTestId('navbar')).toHaveTextContent('Home');
-    expect(getByTestId('navbar')).toHaveTextContent('Quote');
-    expect(getByTestId('navbar')).toHaveTextContent('Calculator');
-  });
-  
+  const { getByTestId } = render(
+    <Router>
+      <Navbar />
+    </Router>,
+  );
+  expect(getByTestId('navbar')).toHaveTextContent('Math Magicians');
+  expect(getByTestId('navbar')).toHaveTextContent('Home');
+  expect(getByTestId('navbar')).toHaveTextContent('Quote');
+  expect(getByTestId('navbar')).toHaveTextContent('Calculator');
+});
