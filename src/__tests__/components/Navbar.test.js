30f987cc45f6e53997e960cf0b9ee633ba754f89
ZawadiSumba66 <rosesumba98@gmail.com>
2021-06-01 21:15:37 +0300

    Add tests for the navbar

--- src/__tests__/components/Navbar.test.js
+++ src/__tests__/components/Navbar.test.js
@@ -1,4 +1,19 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import Display from '../../components/Display';
+import Navbar from '../../components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
 
+
+it('should render the correct buttons for the calculator', () => {
+    
+    const { getByTestId } = render(
+        <Router>
+          <Navbar />
+        </Router>
+      );
+    expect(getByTestId('navbar')).toHaveTextContent('Math Magicians');
+    expect(getByTestId('navbar')).toHaveTextContent('Home');
+    expect(getByTestId('navbar')).toHaveTextContent('Quote');
+    expect(getByTestId('navbar')).toHaveTextContent('Calculator');
+  });
+  
